4f94fccfc024cef7342c74c38f22399f81368b9f
yutanj <yuta.bstars@gmail.com>
2022-08-12 18:46:49 +0900

    ginでwebアプリ

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+	/*
+	   router.GET("/", func(ctx *gin.Context){
+	       ctx.HTML(200, "index.html", gin.H{})
+	   })
+	*/
+	data := "Hello Go/Gin!!"
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{"data": data})
+	})
+
+	router.Run()
 }
